81f34c2a52b52f39e2e2fcb7db6f0e82fc512ec4
michaelpf97 <michaelpf97@gmail.com>
2021-09-07 20:16:13 +0100

    Completed Day 2

---
+++ day_2/src/main.rs
@@ -0,0 +1,31 @@
+use std::fs;
+use itertools::Itertools;
+
+fn main() {
+    let data = read_input("input.txt");
+
+    let answer = find_answer(data);
+
+    println!("The answer is {}", {answer.0 * answer.1 * answer.2});
+}
+
+fn read_input(filename: &str) -> Vec<u32> {
+
+    let contents = fs::read_to_string(filename)
+        .expect("Error reading file");
+
+    let contents: Vec<u32> = contents
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .map(|s| s.parse().expect("Please input a number"))
+        .collect();
+
+    contents
+}
+
+fn find_answer(vector: Vec<u32>) -> (u32, u32, u32) {
+    for (a, b, c) in vector.iter().tuple_combinations() {
+        if a + b + c == 2020 { return (*a, *b, *c) }
+    }
+    panic!("No combination found");
+}
