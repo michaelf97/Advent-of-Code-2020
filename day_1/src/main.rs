f2d630b692f37715abae2588704dc52b5fe6e868
michaelpf97 <michaelpf97@gmail.com>
2021-09-08 14:15:00 +0100

    Refactored for parts

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,31 +1,43 @@
 use std::fs;
 use itertools::Itertools;
 
-fn main() {
-    let data = read_input("input.txt");
+struct Problem {}
 
-    let answer = find_answer(data);
+impl Problem {
+    fn run(filename: &str) {
+        let data = Problem::read_input(filename);
 
-    println!("The answer is {}", {answer.0 * answer.1});
-}
+        Problem::part_1(&data);
 
-fn read_input(filename: &str) -> Vec<u32> {
+        Problem::part_2(&data)
+    }
 
-    let contents = fs::read_to_string(filename)
+    fn read_input(filename: &str) -> Vec<u32> {
+        let contents = fs::read_to_string(filename)
         .expect("Error reading file");
 
-    let contents: Vec<u32> = contents
-        .split_whitespace()
-        .map(|s| s.to_string())
-        .map(|s| s.parse().expect("Please input a number"))
-        .collect();
+        let contents: Vec<u32> = contents
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .map(|s| s.parse().expect("Please input a number"))
+            .collect();
 
-    contents
+        contents
+    }
+
+    fn part_1(vector: &Vec<u32>) {
+        for (a, b) in vector.iter().tuple_combinations() {
+            if a + b == 2020 { println!("Part 1 Answer: {}", *a * *b) }
+        }
+    }
+
+    fn part_2(vector: &Vec<u32>) {
+        for (a, b, c) in vector.iter().tuple_combinations() {
+            if a + b + c == 2020 { println!("Part 1 Answer: {}", *a * *b * *c) }
+        }
+    }
 }
 
-fn find_answer(vector: Vec<u32>) -> (u32, u32) {
-    for (a, b) in vector.iter().tuple_combinations() {
-        if a + b == 2020 { return (*a, *b) }
-    }
-    panic!("No combination found");
+fn main() {
+    Problem::run("input.txt")
 }
