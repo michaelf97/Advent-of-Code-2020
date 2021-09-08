b16eaafa78c9e5c3d7f75f2792921fd10b6bd324
michaelpf97 <michaelpf97@gmail.com>
2021-09-08 14:45:19 +0100

    Refactored for parts

--- day_2/src/main.rs
+++ day_2/src/main.rs
@@ -1,31 +1,87 @@
 use std::fs;
-use itertools::Itertools;
+use itertools::izip;
+
+#[derive(Debug)]
+struct PasswordProfile<'a> {
+    min: usize,
+    max: usize,
+    constraint: char,
+    password: &'a str,
+}
+
+impl PasswordProfile<'_> {
+    fn passed_old(&self) -> bool {
+        let count = self.password.chars().filter(|c| *c == self.constraint).count();
+        count >= self.min && count <= self.max
+    }
+    fn passed(&self) -> bool {
+        let password = self.password.chars().collect::<Vec<char>>();
+        let char_list: [Option<&char>; 2] = [
+            password.get(self.min - 1),
+            password.get(self.max - 1),
+        ];
+        return char_list[0] != char_list[1] && (char_list[0] == Some(&self.constraint) || char_list[1] == Some(&self.constraint))
+    }
+}
+
+struct Problem {}
+
+impl Problem {
+    fn run(filename: &str) {
+        let data = Problem::read_input(filename);
+
+        Problem::part_1(&data);
+
+        Problem::part_2(&data);
+    }
+
+    fn read_input(filename: &str) -> Vec<String> {
+
+        let contents = fs::read_to_string(filename)
+            .expect("Error reading file");
+    
+        let contents: Vec<String> = contents
+            .split_whitespace()
+            .map(|s| s.to_string())
+            .collect();
+    
+        contents
+    }
+
+    fn part_1(vector: &Vec<String>) {
+
+        let mut counter: usize = 0;
+    
+        for (a, b, c) in izip!(vector, &vector[1..], &vector[2..]).step_by(3) {
+            let profile = PasswordProfile {
+                min: a.split("-").collect::<Vec<&str>>()[0].parse().expect("Minimum constraint is not a number"),
+                max: a.split("-").collect::<Vec<&str>>()[1].parse().expect("Maximum constraint is not a number"),
+                constraint: b.chars().next().unwrap(),
+                password: &c,
+                };
+            if profile.passed_old() == false { counter += 1 }
+        };
+        println!("Part 1 Answer: {}", counter)
+    }
+
+    fn part_2(vector: &Vec<String>) {
+
+        let mut counter: usize = 0;
+
+        for (a, b, c) in izip!(vector, &vector[1..], &vector[2..]).step_by(3) {
+            let profile = PasswordProfile {
+                min: a.split("-").collect::<Vec<&str>>()[0].parse().expect("Minimum constraint is not a number"),
+                max: a.split("-").collect::<Vec<&str>>()[1].parse().expect("Maximum constraint is not a number"),
+                constraint: b.chars().next().unwrap(),
+                password: &c,
+                };
+            if profile.passed() == true { counter += 1 }
+            };
+        
+        println!("Part 2 Answer: {}", counter);
+    }
+}
 
 fn main() {
-    let data = read_input("input.txt");
-
-    let answer = find_answer(data);
-
-    println!("The answer is {}", {answer.0 * answer.1 * answer.2});
-}
-
-fn read_input(filename: &str) -> Vec<u32> {
-
-    let contents = fs::read_to_string(filename)
-        .expect("Error reading file");
-
-    let contents: Vec<u32> = contents
-        .split_whitespace()
-        .map(|s| s.to_string())
-        .map(|s| s.parse().expect("Please input a number"))
-        .collect();
-
-    contents
-}
-
-fn find_answer(vector: Vec<u32>) -> (u32, u32, u32) {
-    for (a, b, c) in vector.iter().tuple_combinations() {
-        if a + b + c == 2020 { return (*a, *b, *c) }
-    }
-    panic!("No combination found");
+    Problem::run("input.txt")
 }
