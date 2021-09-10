1af046a12f61e911c0648a6c3ebc2d139702fa65
michaelpf97 <michaelpf97@gmail.com>
2021-09-10 04:55:56 +0100

    Improved part 1 and completed part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -1,40 +1,103 @@
-use std::fs;
+struct Line {
+    gradient: f32,
+    y_intercept: f32,
+    step_by: usize,
+}
 
-struct Problem {}
-
-impl Problem {
-    fn run(filename: &str) {
-        let data = Problem::read_input(filename);
-
-        println!("Answer for part 1: {}", Problem::part_1(&data, 3, 1));
+impl Line {
+    fn x_coordinate(&self, y_coordinate: f32) -> f32 {
+        (y_coordinate - self.y_intercept) / self.gradient
     }
 
-    fn read_input(filename: &str) -> Vec<String> {
+    fn y_coordinate(&self, x_coordinate: f32) -> f32 {
+        (self.gradient * x_coordinate) + self.y_intercept
+    }
+
+    fn calculate_gradient(x_coordinate: f32, y_coordinate: f32) -> f32 {
+        y_coordinate/x_coordinate
+    }
+}
+
+mod problem {
+    use std::fs;
+    use super::Line;
+
+    fn read_input(filename: &str) -> Vec<Vec<char>> {
+
         let contents = fs::read_to_string(filename)
             .expect("Error reading file");
 
-        let contents: Vec<String> = contents
+        let contents: Vec<Vec<char>> = contents
             .split_whitespace()
-            .map(|s| s.to_string())
-            .collect();
+            .map(|s| s.chars().collect())
+            .collect::<Vec<Vec<char>>>();
 
         contents
     }
 
-    fn part_1(vector: &Vec<String>, x_gradient: usize, y_gradient: usize) -> usize {
-        let mut counter = 0;
+    fn problem_1(plot: &Vec<Vec<char>>) {
+        let line = Line {
+            gradient: Line::calculate_gradient(3.0, 1.0),
+            y_intercept: 0.0,
+            step_by: 1,
+        };
+        count(&plot, '#', &line);
+    }
 
-        for (y_coordinate, x_axis) in vector.iter().enumerate().step_by(y_gradient) {
-            let mut x_coordinate = y_coordinate * x_gradient;
-            if x_coordinate != 0 { x_coordinate = x_coordinate % x_axis.len() }
-            if x_axis.chars().collect::<Vec<char>>().get(x_coordinate) == Some(&'#') { 
-                counter += 1;
+    fn problem_2(plot: &Vec<Vec<char>>) {
+        let mut answer = 1;
+
+        let lines = vec![
+            Line {
+                gradient: Line::calculate_gradient(1.0, 1.0),
+                y_intercept: 0.0,
+                step_by: 1,
+            },
+            Line {
+                gradient: Line::calculate_gradient(3.0, 1.0),
+                y_intercept: 0.0,
+                step_by: 1,
+            },
+            Line {
+                gradient: Line::calculate_gradient(5.0, 1.0),
+                y_intercept: 0.0,
+                step_by: 1,
+            },
+            Line {
+                gradient: Line::calculate_gradient(7.0, 1.0),
+                y_intercept: 0.0,
+                step_by: 1,
+            },
+            Line {
+                gradient: Line::calculate_gradient(1.0, 2.0),
+                y_intercept: 0.0,
+                step_by: 2,
             }
+        ];
+
+        for line in lines { answer *= count(&plot, '#', &line) }
+        println!("Answer for problem 2: {}", answer);
+    }
+
+    fn count(plot: &Vec<Vec<char>>, character: char, line: &Line) -> usize {
+        let mut counter: usize = 0;
+
+        for y_coordinate in (0..plot.len()).step_by(line.step_by) {
+            let x_coordinate = line.x_coordinate(y_coordinate as f32).ceil() as usize % plot[0].len();
+            if plot[y_coordinate][x_coordinate] == character { counter += 1 }
         }
         counter
+    }
+
+    pub fn run() {
+        let data = read_input("input.txt");
+        problem_1(&data);
+        problem_2(&data);
     }
 }
 
 fn main() {
-    Problem::run("input.txt")
+    problem::run();
+    let test: f32 = 9.999999;
+    println!("{}", test.floor() as usize);
 }
