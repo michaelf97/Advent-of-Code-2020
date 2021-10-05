cbb7ac05a072a17627702e8af77ab7cfcb90fead
michaelpf97 <michaelpf97@gmail.com>
2021-10-05 13:17:18 +0100

    Finished day 5 part 1

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,5 +1,6 @@
 use std::convert::TryFrom;
 
+#[derive(Debug)]
 struct TwoDSpace {
     x_dimension: Vec<usize>,
     y_dimension: Vec<usize>,
@@ -13,6 +14,14 @@ impl TwoDSpace {
         }
     }
 
+    fn id(&self) -> usize {
+        if self.x_dimension.len() == 1 && self.y_dimension.len() == 1 {
+            (self.x_dimension.get(0).unwrap() * 8) + self.y_dimension.get(0).unwrap()
+        } else {
+            panic!("ID not calculable")
+        }
+    }
+
     fn operation(&mut self, command: Command) {
         match command {
             Command::Forward | Command::Back => self.x_operation(&command),
@@ -21,11 +30,39 @@ impl TwoDSpace {
     }
 
     fn y_operation(&mut self, command: &Command) {
-        println!("{:?}", command);
-    }
-    
-    fn x_operation(&mut self, command: &Command) {
+        let chunks: Vec<Vec<usize>> = self
+            .y_dimension
+            .chunks(self.y_dimension.len() / 2)
+            .map(|u| u.to_owned())
+            .collect();
 
+        if chunks.len() > 2 {
+            panic!("Unexpected dimension size {:?}", chunks)
+        }
+
+        self.y_dimension = match command {
+            Command::Left => chunks.get(0).unwrap().to_owned(),
+            Command::Right => chunks.get(1).unwrap().to_owned(),
+            _ => panic!("Unxpected Command enumeration, {:?}", command),
+        }
+    }
+
+    fn x_operation(&mut self, command: &Command) {
+        let chunks: Vec<Vec<usize>> = self
+            .x_dimension
+            .chunks(self.x_dimension.len() / 2)
+            .map(|u| u.to_owned())
+            .collect();
+
+        if chunks.len() > 2 {
+            panic!("Unexpected dimension size {:?}", chunks)
+        }
+
+        self.x_dimension = match command {
+            Command::Forward => chunks.get(0).unwrap().to_owned(),
+            Command::Back => chunks.get(1).unwrap().to_owned(),
+            _ => panic!("Unxpected Command enumeration, {:?}", command),
+        }
     }
 }
 
@@ -51,7 +88,6 @@ impl TryFrom<char> for Command {
     }
 }
 
-
 mod problem {
     use super::Command;
     use super::TwoDSpace;
@@ -61,14 +97,21 @@ mod problem {
     use std::path::Path;
 
     fn problem_1(data: &Vec<String>) {
+        let mut answer: usize = 0;
+
         for tree in data {
             let mut space = TwoDSpace::new();
 
             tree.chars().for_each(|t| {
-                let command = Command::try_from(t);
+                space.operation(Command::try_from(t).unwrap());
             });
 
+            if space.id() > answer {
+                answer = space.id();
+            }
         }
+
+        println!("Answer for problem 1: {}", answer);
     }
 
     fn problem_2() {}
