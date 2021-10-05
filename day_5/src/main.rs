cccd04bbcc6804193537fe0af0175b676addc198
michaelpf97 <michaelpf97@gmail.com>
2021-10-05 11:46:56 +0100

    Progress

--- day_5/src/main.rs
+++ day_5/src/main.rs
@@ -1,8 +1,35 @@
+use std::convert::TryFrom;
+
 struct TwoDSpace {
     x_dimension: Vec<usize>,
     y_dimension: Vec<usize>,
 }
 
+impl TwoDSpace {
+    fn new() -> TwoDSpace {
+        TwoDSpace {
+            x_dimension: (0..128).collect(),
+            y_dimension: (0..8).collect(),
+        }
+    }
+
+    fn operation(&mut self, command: Command) {
+        match command {
+            Command::Forward | Command::Back => self.x_operation(&command),
+            Command::Left | Command::Right => self.y_operation(&command),
+        }
+    }
+
+    fn y_operation(&mut self, command: &Command) {
+        println!("{:?}", command);
+    }
+    
+    fn x_operation(&mut self, command: &Command) {
+
+    }
+}
+
+#[derive(Debug)]
 enum Command {
     Forward,
     Back,
@@ -10,31 +37,37 @@ enum Command {
     Right,
 }
 
+impl TryFrom<char> for Command {
+    type Error = &'static str;
+
+    fn try_from(character: char) -> Result<Self, Self::Error> {
+        match character {
+            'F' => Ok(Self::Forward),
+            'B' => Ok(Self::Back),
+            'L' => Ok(Self::Left),
+            'R' => Ok(Self::Right),
+            _ => Err("Invalid command"),
+        }
+    }
+}
+
+
 mod problem {
-    use super::TwoDSpace;
     use super::Command;
+    use super::TwoDSpace;
+    use std::convert::TryFrom;
     use std::fs::File;
     use std::io::{BufRead, BufReader, Error, Lines};
     use std::path::Path;
 
     fn problem_1(data: &Vec<String>) {
         for tree in data {
-            let mut space = TwoDSpace{
-                x_dimension: (0..128).collect(),
-                y_dimension: (0..8).collect(),
-            };
-
-            let mut command: Command;
+            let mut space = TwoDSpace::new();
 
             tree.chars().for_each(|t| {
-                match t {
-                    'F' => command = Command::Forward,
-                    'B' => command = Command::Back,
-                    'L' => command = Command::Left,
-                    'R' => command = Command::Right,
-                    _ => println!("Unknown command"),
-                }
-            })
+                let command = Command::try_from(t);
+            });
+
         }
     }
 
@@ -66,4 +99,6 @@ mod problem {
 
 fn main() {
     problem::run();
+
+    let test = String::from("Hello");
 }
